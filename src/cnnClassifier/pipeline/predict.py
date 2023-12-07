bcded4324029d93d8320c5bbacaf3a4ef8b0a567
Dhyey2901 <dhyeyvyas2003@gmail.com>
2023-12-07 17:09:37 +0530

    prediction

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -3,25 +3,25 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-class PredicationPipeline:
-    def __init__(self,filename):
-        self.filename=filename
 
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
 
     def predict(self):
-        #load model
-        model = load_model(os.path.join("artifacts","training","model.h5"))
+        # load model
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
         imagename = self.filename
-        test_image = image.load_image(imagename, targetsize=(224,224))
+        test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image,axis=0)
-        result = np.argmax(model.predict(test_image),axis=1)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
         if result[0] == 1:
-            prediction = "HEALTHY"
+            prediction = 'Healthy'
             return [{"image": prediction}]
-        else
-            prediction: "COCCIDIOSIS"
+        else:
+            prediction = 'Coccidiosis'
             return [{"image": prediction}]
